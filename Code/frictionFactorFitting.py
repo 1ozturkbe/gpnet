d2ab5fdc2355ee051dd0ebdfe9900812822cd268
1ozturkbe <1ozturkbe@gmail.com>
2018-09-17 16:10:58 -0400

    Working friction factor fits.

--- Code/frictionFactorFitting.py
+++ Code/frictionFactorFitting.py
@@ -1,30 +1,22 @@
 from gpfit.fit import fit
 import numpy as np
-from numpy import logspace, log, random, vstack, hstack
+from numpy import logspace, log, meshgrid
 # fixed initial guess for fitting
-Re = list(logspace(3, 8, 1001))
-Re = hstack(Re)
-Re = Re.astype(np.float)
+Re = logspace(3, 8, 50)
+relRough = logspace(-5, -1, 50)
+Re, relRough = meshgrid(Re, relRough)
+Re = Re.flatten()
+relRough = relRough.flatten()
 
-relRough = list(logspace(-5, -1, 1001))
-relRough = hstack(relRough)
-relRough = relRough.astype(np.float)
+f = 0.25/log(relRough/3.7 + 5.74/Re**0.9)**2 
 
-f = list(0.25/log(relRough/3.7 + 5.74/Re**0.9)**2)
-f = hstack(f)
-f = f.astype(np.float)
-
-x = log(Re)
-y = log(relRough)
-z = log(f)
-
-xData = [Re, relRough]
-xData = np.log(xData)
-
+x = [Re, relRough]
+x = np.log(x)
+y = np.log(f)
 K = 3
-cMA, errorMA = fit(xData, z, K, "MA")
-cSMA, errorSMA = fit(xData, z, K, "SMA")
-cISMA, errorISMA = fit(xData, z, K, "ISMA")
+cMA, errorMA = fit(x, y, K, "MA")
+cSMA, errorSMA = fit(x, y, K, "SMA")
+cISMA, errorISMA = fit(x, y, K, "ISMA")
 print "MA RMS Error: %.5g" % errorMA
 print "SMA RMS Error: %.5g" % errorSMA
 print "ISMA RMS Error: %.5g" % errorISMA
