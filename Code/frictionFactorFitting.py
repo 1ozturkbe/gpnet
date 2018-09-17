6c525aa0b611a119d5beb59602f9ddcf305ac7c5
1ozturkbe <1ozturkbe@gmail.com>
2018-09-17 15:32:54 -0400

    Fits are still not working...

--- Code/frictionFactorFitting.py
+++ Code/frictionFactorFitting.py
@@ -1,14 +1,26 @@
 from gpfit.fit import fit
-from numpy import logspace, log, random, vstack
+import numpy as np
+from numpy import logspace, log, random, vstack, hstack
 # fixed initial guess for fitting
-random.seed(33404)
-Re = logspace(3, 8, 1001)
-relRough = logspace(-5, -1, 1001)
-f = 0.25/log(relRough/3.7 + 5.74/Re**0.9)**2
+Re = list(logspace(3, 8, 1001))
+Re = hstack(Re)
+Re = Re.astype(np.float)
+
+relRough = list(logspace(-5, -1, 1001))
+relRough = hstack(relRough)
+relRough = relRough.astype(np.float)
+
+f = list(0.25/log(relRough/3.7 + 5.74/Re**0.9)**2)
+f = hstack(f)
+f = f.astype(np.float)
+
 x = log(Re)
 y = log(relRough)
 z = log(f)
-xData = vstack((x, y))
+
+xData = [Re, relRough]
+xData = np.log(xData)
+
 K = 3
 cMA, errorMA = fit(xData, z, K, "MA")
 cSMA, errorSMA = fit(xData, z, K, "SMA")
