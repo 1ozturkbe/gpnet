a3e36ee1e4c98d1bb61a944b84ad80d4f42f5c8e
1ozturkbe <1ozturkbe@gmail.com>
2019-11-06 11:49:25 -0500

    Making sure data is zero-indexed.

--- gpnet/read_data.py
+++ gpnet/read_data.py
@@ -7,7 +7,7 @@ def real_index(str_index):
         str_num = [i for i in str_index if i.isdigit()]
         return int(''.join(str_num))
 
-def read_inp(file_directory):
+def read_inp(file_directory, min_junc_index=0, min_pipe_index=0):
     file = open(file_directory, 'r')
     lines = file.readlines()
     file.close()
@@ -43,32 +43,32 @@ def read_inp(file_directory):
             for i in range(len(relevant_data)):
                 junction_index, elev, demand = relevant_data[i][0:3]
                 junction_index = real_index(junction_index)
-                elevation[int(junction_index)-1] = float(elev)
-                sinks[int(junction_index)-1] = float(demand)
+                elevation[int(junction_index)-min_junc_index] = float(elev)
+                sinks[int(junction_index)-min_junc_index] = float(demand)
         elif marker == r:
             for i in range(len(relevant_data)):
                 junction_index, headpress = relevant_data[i][0:2]
                 junction_index = real_index(junction_index)
-                head[int(junction_index)-1] = float(headpress)
-                elevation[int(junction_index)-1] = float(headpress)
+                head[int(junction_index)-min_junc_index] = float(headpress)
+                elevation[int(junction_index)-min_junc_index] = float(headpress)
         elif marker == c:
             for i in range(len(relevant_data)):
                 junction_index, x, y = relevant_data[i][0:3]
                 junction_index = real_index(junction_index)
-                coordinates[int(junction_index)-1] = [float(x), float(y)]
+                coordinates[int(junction_index)-min_junc_index] = [float(x), float(y)]
         elif marker == p:
             for i in range(len(relevant_data)):
                 pipe_ID, node1, node2, length, diameter, rough = relevant_data[i][0:6]
-                pipe_ID = real_index(pipe_ID)-1
-                node1 = real_index(node1)-1
-                node2 = real_index(node2)-1
+                pipe_ID = real_index(pipe_ID)-min_pipe_index
+                node1 = real_index(node1)-min_junc_index
+                node2 = real_index(node2)-min_junc_index
                 topology_dict[pipe_ID] = [node1, node2]
                 L[pipe_ID] = float(length)
                 D[pipe_ID] = float(diameter)
                 roughness[pipe_ID] = float(rough)
-    if len(coordinates) != len(elevation):
-        raise ValueError('Dimensionality mismatch in coordinate and elevation data!')
-    elif len(sinks) + len(head) != len(coordinates):
+    if len(roughness) != len(L) or len(D) != len(roughness):
+        raise ValueError('Dimensionality mismatch in pipe data!')
+    elif len(sinks) + len(head) != len(elevation):
         raise ValueError('Problem is over/underdefined in demand and head pressure!')
     return elevation, head, sinks, coordinates, topology_dict, L, D, roughness
 
