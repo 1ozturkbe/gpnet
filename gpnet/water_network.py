d571d4489d940a7dd7c27ba9db4747beef678391
1ozturkbe <1ozturkbe@gmail.com>
2019-11-06 09:11:36 -0500

    Can run models from input file or arrays. Can't figure out why results are different though.

--- gpnet/water_network.py
+++ gpnet/water_network.py
@@ -10,14 +10,32 @@ from robust.robust import RobustModel
 if __name__ == '__main__':
     # Changing Tightness requirement to within 0.1%
     Tight.reltol = 10.**-3
-    m = hanoi(friction='HW')
-    warm_start = {m["D"]: (1.016 - 0.3048)*np.random.rand(len(m.topology_list)) + 0.3048}
-    sol = m.localsolve(verbosity=2, reltol=1e-2, iteration_limit=150, x0=warm_start)
-    print(sol['cost'])
+    m1 = hanoi_from_data(friction='DW')
+    warm_start = {m1["D"]: (1.016 - 0.3048)*np.random.rand(len(m1.topology_dict)) + 0.3048}
+    sol1 = m1.localsolve(verbosity=2, reltol=1e-2, iteration_limit=150, x0=warm_start)
+    print(sol1['cost'])
 
-    draw_KT_network(sol, m.coordinates, m.topology_list)
+    m2 = hanoi(friction='DW')
+    warm_start = {m2["D"]: (1.016 - 0.3048)*np.random.rand(len(m2.topology_dict)) + 0.3048}
+    sol2 = m2.localsolve(verbosity=2, reltol=1e-2, iteration_limit=150, x0=warm_start)
+    print(sol2['cost'])
+
+    m1 = hanoi_from_data(friction='HW')
+    warm_start = {m1["D"]: (1.016 - 0.3048)*np.random.rand(len(m1.topology_dict)) + 0.3048}
+    sol1 = m1.localsolve(verbosity=2, reltol=1e-2, iteration_limit=150, x0=warm_start)
+    print(sol1['cost'])
+
+    m2 = hanoi(friction='HW')
+    warm_start = {m2["D"]: (1.016 - 0.3048)*np.random.rand(len(m2.topology_dict)) + 0.3048}
+    sol2 = m2.localsolve(verbosity=2, reltol=1e-2, iteration_limit=150, x0=warm_start)
+    print(sol2['cost'])
+
+    sol1.diff(sol2)
+
+
+    draw_KT_network(sol, m.coordinates, m.topology_dict)
 
     # Comparing against robust solution
-    rm = RobustModel(m, 'elliptical', nominalsolve=sol, gamma=1)
-    rmsol = rm.robustsolve()
-    draw_KT_network(rmsol, m.coordinates, m.topology_list)
+    # rm = RobustModel(m, 'elliptical', nominalsolve=sol, gamma=1)
+    # rmsol = rm.robustsolve()
+    # draw_KT_network(rmsol, m.coordinates, m.topology_list)
